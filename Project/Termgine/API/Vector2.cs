37f91ddb28dceeab15f0efc699abee86a939e673
Morasiu <moras2601@wp.pl>
2018-12-14 12:20:31 +0100

    '+' operator overloading for Vector2

--- Project/Termgine/API/Vector2.cs
+++ Project/Termgine/API/Vector2.cs
@@ -1,4 +1,6 @@
-﻿namespace Termgine {
+﻿using System.Runtime.InteropServices;
+
+namespace Termgine {
   public class Vector2 {
     public static Vector2 Zero => new Vector2(0, 0);
 
@@ -9,6 +11,10 @@
     public Vector2(int x, int y) {
       X = x;
       Y = y;
+    }
+
+    public static Vector2 operator +(Vector2 v1, Vector2 v2) {
+      return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
     }
   }
 }
