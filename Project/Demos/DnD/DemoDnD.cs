b4fe06adffedaf947dc882e414998c66f7dcbb94
Morasiu <morasiu2@gmail.com>
2018-12-27 17:59:42 +0100

    Updated DnD demo

--- Project/Demos/DnD/DemoDnD.cs
+++ Project/Demos/DnD/DemoDnD.cs
@@ -24,7 +24,7 @@ namespace Termgine {
     private static Scene SetupDragonTalkScene(){
       var scene = new Scene();
       var dragonFace = new Image(Vector2.Zero, File.ReadAllText("Resources/dragonFace.aa"), '1');
-      dragonFace.Position = display.Center - dragonFace.GetHeight()/2 + Vector2.Right * 20;
+      dragonFace.Position = display.Center - dragonFace.Width/2 + Vector2.Right * 20;
       var talk = "You are finally awake.\n" + 
                  "I'm Malfor, the Elder Red Dragon.\n" +
                  "You are not from this world.\n" + 
@@ -40,14 +40,15 @@ namespace Termgine {
       var rightDragon = new Image(Vector2.Zero, File.ReadAllText("Resources/rightDragon.aa"), '1');
       var leftDragon = new Image(Vector2.Zero, File.ReadAllText("Resources/leftDragon.aa"), '1');
       var scene = new Scene();
-      var welcome = new Label("  Welcome to D&D  \n"+
-                              " Terminal Edition "); 
-      welcome.Position = display.Center - new Vector2(welcome.GetWidth()/2, 0);
-      welcome.SetColor('1');
+      var welcome = new Label(Vector2.Zero,
+                              "  Welcome to D&D  \n"+
+                              " Terminal Edition ",
+                              '1'); 
+      welcome.Position = display.Center - new Vector2(welcome.Width/2, 0);
       var welcomeWithBorder = new Border(welcome, '6', BorderType.DoubleExtended);
-      rightDragon.Position = welcomeWithBorder.Position - new Vector2(-welcomeWithBorder.GetWidth() - 1, rightDragon.GetHeight()/2);
-      leftDragon.Position = new Vector2(rightDragon.Position.X - welcomeWithBorder.GetWidth() - leftDragon.GetWidth() - 1, rightDragon.Position.Y);
-      var pressAnyKey = new Label("Press any key to start...");
+      rightDragon.Position = welcomeWithBorder.Position - new Vector2(-welcomeWithBorder.Width - 1, rightDragon.Height/2);
+      leftDragon.Position = new Vector2(rightDragon.Position.X - welcomeWithBorder.Width - leftDragon.Width - 1, rightDragon.Position.Y);
+      var pressAnyKey = new Label(Vector2.Zero, "Press any key to start...", '5');
       pressAnyKey.Position = welcomeWithBorder.Position + Vector2.Down * 7 + Vector2.Left;
       scene.AddObject(welcomeWithBorder);
       scene.AddObject(rightDragon);
