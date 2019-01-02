c216aae0b646c39da1a0985dbecb2acd1443544b
Morasiu <morasiu2@gmail.com>
2019-01-02 13:17:19 +0100

    Added tests (finally)

--- Project/Termgine/API/Vector2.cs
+++ Project/Termgine/API/Vector2.cs
@@ -21,15 +21,16 @@ namespace Termgine {
     public int X { get; set; }
 
     public int Y { get; set; }
-    # endregion
+
+    #endregion
 
     #region Public methods
     public override bool Equals(object obj) {
       if (!(obj is Vector2))
         return false;
-      return this == (Vector2) obj;
+      return this == (Vector2)obj;
     }
-    
+
     public override int GetHashCode() => (this.X, this.Y).GetHashCode();
     #endregion
 
@@ -37,7 +38,7 @@ namespace Termgine {
     // Vector2, Vector2
     public static Vector2 operator +(Vector2 v1, Vector2 v2) => new Vector2(v1.X + v2.X, v1.Y + v2.Y);
     public static Vector2 operator -(Vector2 v1, Vector2 v2) => new Vector2(v1.X - v2.X, v1.Y - v2.Y);
-    public static Vector2 operator *(Vector2 v1, Vector2 v2) => new Vector2(v1.X * v2.X, v1.Y - v2.Y);
+    public static Vector2 operator *(Vector2 v1, Vector2 v2) => new Vector2(v1.X * v2.X, v1.Y * v2.Y);
     public static Vector2 operator /(Vector2 v1, Vector2 v2) => new Vector2(v1.X / v2.X, v1.Y / v2.Y);
     public static bool operator ==(Vector2 v1, Vector2 v2) => v1.X == v2.X && v1.Y == v2.Y;
     public static bool operator !=(Vector2 v1, Vector2 v2) => v1.X != v2.X || v1.Y != v2.Y;
@@ -47,23 +48,22 @@ namespace Termgine {
     public static Vector2 operator *(Vector2 v1, int i) => new Vector2(v1.X * i, v1.Y * i);
     public static Vector2 operator /(Vector2 v1, int i) => new Vector2(v1.X / i, v1.Y / i);
     //int, Vector2
-    public static Vector2 operator +(int i, Vector2 v1) => new Vector2(i + v1.X, i + v1.Y);    
+    public static Vector2 operator +(int i, Vector2 v1) => new Vector2(i + v1.X, i + v1.Y);
     public static Vector2 operator -(int i, Vector2 v1) => new Vector2(i - v1.X, i - v1.Y);
     public static Vector2 operator *(int i, Vector2 v1) => new Vector2(i * v1.X, i * v1.Y);
     public static Vector2 operator /(int i, Vector2 v1) => new Vector2(i / v1.X, i / v1.Y);
 
     // Vector2, tuple
-    public static Vector2 operator +(Vector2 v1, (int, int) t) => new Vector2(v1.X + t.Item1, v1.Y + t.Item2);
-    public static Vector2 operator -(Vector2 v1, (int, int) t) => new Vector2(v1.X - t.Item1, v1.Y - t.Item2);
-    public static Vector2 operator *(Vector2 v1, (int, int) t) => new Vector2(v1.X * t.Item1, v1.Y * t.Item2);
-    public static Vector2 operator /(Vector2 v1, (int, int) t) => new Vector2(v1.X * t.Item1, v1.Y * t.Item2);
-   // tuple, Vector2
-    public static Vector2 operator +((int, int) t, Vector2 v1) => new Vector2(t.Item1 + v1.X, t.Item2 + v1.Y);    
-    public static Vector2 operator -((int, int) t, Vector2 v1) => new Vector2(t.Item1 - v1.X, t.Item2 - v1.Y);
-    public static Vector2 operator *((int, int) t, Vector2 v1) => new Vector2(t.Item1 * v1.X, t.Item2 * v1.Y);
-    public static Vector2 operator /((int, int) t, Vector2 v1) => new Vector2(t.Item1 * v1.X, t.Item2 * v1.Y);
+    public static Vector2 operator +(Vector2 v1, (int, int)t) => new Vector2(v1.X + t.Item1, v1.Y + t.Item2);
+    public static Vector2 operator -(Vector2 v1, (int, int)t) => new Vector2(v1.X - t.Item1, v1.Y - t.Item2);
+    public static Vector2 operator *(Vector2 v1, (int, int)t) => new Vector2(v1.X * t.Item1, v1.Y * t.Item2);
+    public static Vector2 operator /(Vector2 v1, (int, int)t) => new Vector2(v1.X * t.Item1, v1.Y * t.Item2);
+    // tuple, Vector2
+    public static Vector2 operator +((int, int)t, Vector2 v1) => new Vector2(t.Item1 + v1.X, t.Item2 + v1.Y);
+    public static Vector2 operator -((int, int)t, Vector2 v1) => new Vector2(t.Item1 - v1.X, t.Item2 - v1.Y);
+    public static Vector2 operator *((int, int)t, Vector2 v1) => new Vector2(t.Item1 * v1.X, t.Item2 * v1.Y);
+    public static Vector2 operator /((int, int)t, Vector2 v1) => new Vector2(t.Item1 * v1.X, t.Item2 * v1.Y);
 
-
-		#endregion
-	}
+    #endregion
+  }
 }
