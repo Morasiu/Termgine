e7ba83fb184c3b60ca58d7d9750e3fc534bfe2aa
Morasiu <morasiu2@gmail.com>
2019-01-07 16:10:12 +0100

    Tests info for Display and Scene

---
+++ Project/Tests/Termgine.Tests/DisplayTests.cs
@@ -0,0 +1,9 @@
+using System;
+using Xunit;
+
+namespace Termgine.Tests {
+    public class DisplayTests {
+        // IMPORTANT!
+        // Tests not possible becuase of the problem with buffer from Console class!
+    }
+}
---
+++ Project/Tests/Termgine.Tests/SceneTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+
+namespace Termgine.Tests {
+    public class SceneTests {
+        // IMPORTANT!
+        // Tests not possible becuase of the problem with buffer from Console class!
+
+        // [Fact]
+        // public void Contructor_None_ShouldReturnScene() {
+        //     var testScene = new Scene();
+        //     Assert.Equal(Console.WindowHeight, testScene.Height);
+        //     Assert.Equal(Console.WindowWidth, testScene.Width);
+        //     Assert.NotNull(testScene.GameObjects);
+        // }
+
+        // [Fact]
+        // public void GetWidth_None_ShouldReturnCurrentSceneWidth(){
+        //     var testScene = new Scene();
+        //     Assert.Equal(Console.WindowWidth, testScene.Width);
+        // }
+        
+        // [Fact]
+        // public void GetHeight_None_ShouldReturnCurrentSceneHeight(){
+        //     var testScene = new Scene();
+        //     Assert.Equal(Console.WindowHeight, testScene.Height);
+        // }
+
+        // [Fact]
+        // public void SetWidth_100_ShouldSetWidth(){
+        //     var testScene = new Scene();
+        //     testScene.Width = 10;
+        //     Assert.Equal(10, testScene.Width);
+        // }
+    }
+}
